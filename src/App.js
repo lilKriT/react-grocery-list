78d092fbe46ca1835b9c3b62c9e3ba470b5fe6a3
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-04 22:42:52 +0200

    Added local storage. Completed.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -12,6 +21,10 @@ function App() {
     msg: "",
     type: "",
   });
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
