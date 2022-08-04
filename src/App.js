342c995423bf27e1763f566d9c00a98b78619b54
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-04 04:02:22 +0200

    Created the structure.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
 function App() {
-  return <h2>Grocery</h2>;
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Hey");
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery list</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="ex. M&Ms"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">Clear list</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
